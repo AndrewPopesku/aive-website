f243740475f549b81f869010ed575cead000202e
AndrewPopesku <101664066+AndrewPopesku@users.noreply.github.com>
2025-09-25 22:34:02 +0100

    Update and rename next.config.mjs to next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: `/aive-website`,
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -10,13 +11,6 @@ const nextConfig = {
     unoptimized: true,
   },
   output: 'export',
-  distDir: 'out',
-  trailingSlash: true,
-  // Only use basePath/assetPrefix for GitHub Pages build
-  ...(process.env.GITHUB_PAGES && {
-    basePath: '/aive',
-    assetPrefix: '/aive',
-  }),
 }
 
 export default nextConfig
