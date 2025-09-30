3ecae14b3cd7f90d6c3795bc88a6b585a1c9a3f5
Andrii Popesku <andrii.popesku@globalstaffingsupport.com>
2025-09-30 19:44:23 +0100

    chore: update Next.js configuration and package scripts for GitHub Pages support
    - Added conditional basePath and output settings in `next.config.ts` for GitHub Pages deployment.
    - Introduced new build scripts in `package.json` for GitHub Pages and static site serving.
    - Retained ESLint and TypeScript configurations for improved code quality.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,17 +1,21 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig: NextConfig = {
-  // basePath: `/aive-website`,
-  // eslint: {
-  //   ignoreDuringBuilds: true,
-  // },
-  // typescript: {
-  //   ignoreBuildErrors: true,
-  // },
-  // images: {
-  //   unoptimized: true,
-  // },
-  // output: 'export',
+  ...(isGithubPages && {
+    basePath: '/aive-website',
+    output: 'export',
+  }),
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
